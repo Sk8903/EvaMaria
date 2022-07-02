2a3d1d56f7d0800e00abf35984d6d37fc3a6d46a
Sk8903 <85931125+Sk8903@users.noreply.github.com>
2022-07-02 12:53:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -708,7 +708,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"Rᴇǫᴜᴇsᴛᴇᴅ ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : <code>{search}</code>\n\nRᴇǫᴜᴇsᴛᴇᴅ Bʏ : {message.from_user.mention}\n\n\n😌 ɪꜰ ᴛʜᴇ ᴍᴏᴠɪᴇ ʏᴏᴜ ᴀʀᴇ ʟᴏᴏᴋɪɴɢ ꜰᴏʀ ɪs ɴᴏᴛ ᴀᴠᴀɪʟᴀʙʟᴇ ᴛʜᴇɴ ʟᴇᴀᴠᴇ ᴀ ᴍᴇssᴀɢᴇ ʙᴇʟᴏᴡ 😌 \n\nᴇxᴀᴍᴘʟᴇ : \n\nᴇɴᴛᴇʀ ʏᴏᴜʀ ᴍᴏᴠɪᴇ ɴᴀᴍᴇ (ʏᴇᴀʀ) ᴛᴀɢ @admin"
+        cap = f"💖 Rᴇǫᴜᴇsᴛᴇᴅ sᴇʀɪᴀʟ/ᴇᴘɪsᴏᴅᴇ ɴᴏ : <code>{search}</code>\n\n❤️‍🔥Rᴇǫᴜᴇsᴛᴇᴅ Bʏ : {message.from_user.mention}\n\n\n😌 ɪꜰ ᴛʜᴇ sᴇʀɪᴀʟs ʏᴏᴜ ᴀʀᴇ ʟᴏᴏᴋɪɴɢ ꜰᴏʀ ɪs ɴᴏᴛ ᴀᴠᴀɪʟᴀʙʟᴇ ᴛʜᴇɴ ʟᴇᴀᴠᴇ ᴀ ᴍᴇssᴀɢᴇ ʙᴇʟᴏᴡ 😌 \n\nᴇxᴀᴍᴘʟᴇ : \n\nᴇɴᴛᴇʀ ʏᴏᴜʀ sᴇʀɪᴀʟs ɴᴀᴍᴇ (ᴛᴠ ᴄʜᴀɴɴᴇʟ) ᴛᴀɢ @admin"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
