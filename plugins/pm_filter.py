01f0b5b4a8e5f3b44c391648eaa89f44fb38ca33
Sk8903 <85931125+Sk8903@users.noreply.github.com>
2022-07-02 13:10:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -91,16 +91,25 @@ async def next_page(bot, query):
         off_set = offset - 10
     if n_offset == 0:
         btn.append(
+            [
+                InlineKeyboardButton("HOW TO DOWNLOAD", url=f"(https://Jdisk.in/")
+            ],
             [InlineKeyboardButton("⋘ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
              InlineKeyboardButton(f"⌸ Pages {round(int(offset) / 20) + 1} / {round(total / 20)}",
                                   callback_data="pages")]
         )
     elif off_set is None:
         btn.append(
+            [
+                InlineKeyboardButton("HOW TO DOWNLOAD", url=f"(https://Jdisk.in/")
+            ],
             [InlineKeyboardButton(f"⌸ {round(int(offset) / 20) + 1} / {round(total / 20)}", callback_data="pages"),
              InlineKeyboardButton("NEXT ⋙", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
+            [
+                InlineKeyboardButton("HOW TO DOWNLOAD", url=f"(https://Jdisk.in/")
+            ],
             [
                 InlineKeyboardButton("⋘ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
                 InlineKeyboardButton(f"⌸ {round(int(offset) / 20) + 1} / {round(total / 20)}", callback_data="pages"),
@@ -666,11 +675,17 @@ async def auto_filter(client, msg, spoll=False):
         BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
+            [
+                InlineKeyboardButton("HOW TO DOWNLOAD", url=f"(https://Jdisk.in/")
+            ],
             [InlineKeyboardButton(text=f"⌸ 1/{round(int(total_results) / 20)}", callback_data="pages"),
              InlineKeyboardButton(text="NEXT ⋙", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
         btn.append(
+            [
+                InlineKeyboardButton("HOW TO DOWNLOAD", url=f"(https://Jdisk.in/")
+            ],
             [InlineKeyboardButton(text="⌸1/1", callback_data="pages")]
         )
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
