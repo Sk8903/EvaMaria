c33c37471552c7d02890c0a006b69028bbed57e6
Sk8903 <85931125+Sk8903@users.noreply.github.com>
2022-08-21 19:11:27 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -378,3 +378,13 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+shortz = shortzy.Shortzy(DROPLINK_API, "shareus.in")
+
+####################  droplink  ####################
+async def droplink_url(link):
+    print(link)
+    if LONG_DROPLINK_URL == "True" or LONG_DROPLINK_URL is True:
+        return await shortz.get_quick_link(link, silently_fail=True)
+    else:
+        return await shortz.convert(link)
